5f24eac05faf8bd95fad0f229defd7247d0fbc00
Megha Mehta <mmehta.ten@gmail.com>
2022-01-10 07:53:16 +0530

    push code

---
+++ python/Ch 1/definition_start.py
@@ -0,0 +1,16 @@
+# Python Object Oriented Programming by Joe Marini course example
+# Basic class definitions
+
+
+# TODO: create a basic class
+class Book:
+    def __init__(self, title): #initializer function, not a constructor
+        self.title = title
+
+# TODO: create instances of the class
+b1 = Book("Brave new world")
+b2 = Book("War and peace")
+
+# TODO: print the class and property
+print(b1)
+print(b1.title)
