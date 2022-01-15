5d69b1b3eaffe3646e36e7a008863b331353f7f2
Megha Mehta <mmehta.ten@gmail.com>
2022-01-15 10:58:57 +0530

    make class abstract

--- python/Ch 2/abstract_start.py
+++ python/Ch 2/abstract_start.py
@@ -1,11 +1,14 @@
 # Python Object Oriented Programming by Joe Marini course example
 # Using Abstract Base Classes to enforce class constraints
 
+# MEGHA: Import ABC i.e. abstract base class
+from abc import ABC, abstractmethod
 
-class GraphicShape:
+class GraphicShape(ABC):
     def __init__(self):
         super().__init__()
 
+    @abstractmethod
     def calcArea(self):
         pass
 
@@ -19,8 +22,8 @@ class Square(GraphicShape):
     def __init__(self, side):
         self.side = side
 
-
-g = GraphicShape()
+# MEGHA: Comment below because GraphicShape class cannot be instantiated
+# g = GraphicShape()
 
 c = Circle(10)
 print(c.calcArea())
