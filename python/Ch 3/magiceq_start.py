106809d1480d6c4967bb4309c425a1a521f8bb5b
Megha Mehta <mmehta.ten@gmail.com>
2022-01-15 13:41:44 +0530

    implement __eq__

--- python/Ch 3/magiceq_start.py
+++ python/Ch 3/magiceq_start.py
@@ -7,13 +7,28 @@ class Book:
         super().__init__()
         self.title = title
         self.author = author
-        self.price = price
+        self.price = price    
 
     # TODO: the __eq__ method checks for equality between two objects
+    # MEGHA: Below implementation of __eq__() is actually an override of an existing implementation in python
+    def __eq__(self, value):
+        if not isinstance(value,Book):
+            raise ValueError("Can't compare book to a non-book")
+        return (self.title == value.title and
+        self.author == value.author and 
+        self.price == value.price)
 
     # TODO: the __ge__ establishes >= relationship with another obj
+    def __ge__(self, value):
+        if not isinstance(value,Book):
+            raise ValueError("Can't compare book to a non-book")
+        return (self.price >= value.price)
 
     # TODO: the __lt__ establishes < relationship with another obj
+    def __lt__(self, value):
+        if not isinstance(value,Book):
+            raise ValueError("Can't compare book to a non-book")
+        return (self.price < value.price)
 
 
 b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
@@ -22,9 +37,17 @@ b3 = Book("War and Peace", "Leo Tolstoy", 39.95)
 b4 = Book("To Kill a Mockingbird", "Harper Lee", 24.95)
 
 # TODO: Check for equality
+# print(b1 == b3)
+# print(b1 == b2)
+# print(b1 == 42)  # this should throw Value error
 
 
 # TODO: Check for greater and lesser value
-
+# print(b2 >= b1)
+# print(b2 < b1)
 
 # TODO: Now we can sort them too
+books = [b1,b2,b3,b4]
+books.sort()
+
+print([book.title for book in books])
