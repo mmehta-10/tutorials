58952b4f8746d89b877ead563ece4e8eb194845e
Megha Mehta <mmehta.ten@gmail.com>
2022-01-15 11:14:45 +0530

    print class mro

--- python/Ch 2/multiple_start.py
+++ python/Ch 2/multiple_start.py
@@ -6,17 +6,30 @@ class A:
     def __init__(self):
         super().__init__()
         self.foo = "foo"
+        self.name = "Class A"
 
 
 class B:
     def __init__(self):
         super().__init__()
         self.bar = "bar"
+        self.name = "Class B"
 
-
-class C(A, B):
+# MEGHA: Class C lists both classes A and B as its base classes 
+class C(B,A):
     def __init__(self):
         super().__init__()
 
+    def showProps(self):
+        print(self.foo)
+        print(self.bar)
+        print(self.name)
+
 
 c = C()
+
+c.showProps()
+
+# MEGHA: Print method resolution order of Class C
+# Output: (<class '__main__.C'>, <class '__main__.B'>, <class '__main__.A'>, <class 'object'>)
+print(C.__mro__)
