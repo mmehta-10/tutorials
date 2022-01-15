d2f056f7acdfc31b39d90ea1a84d9b73f200d272
Megha Mehta <mmehta.ten@gmail.com>
2022-01-15 12:24:57 +0530

    complete interfaces

--- python/Ch 2/interface_start.py
+++ python/Ch 2/interface_start.py
@@ -3,6 +3,13 @@
 
 from abc import ABC, abstractmethod
 
+class JSONify(ABC):
+    def __init__(self):
+        super().__init__()
+
+    @abstractmethod
+    def toJSON(self):
+        pass
 
 class GraphicShape(ABC):
     def __init__(self):
@@ -13,13 +20,16 @@ class GraphicShape(ABC):
         pass
 
 
-class Circle(GraphicShape):
+class Circle(GraphicShape, JSONify):
     def __init__(self, radius):
         self.radius = radius
 
     def calcArea(self):
         return 3.14 * (self.radius ** 2)
 
+    def toJSON(self):
+        return f"{{\"Circle\" : {str(self.calcArea())} }}"
 
 c = Circle(10)
 print(c.calcArea())
+print(c.toJSON())
