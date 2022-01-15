232c5fa9523b077498ac70e01d0d1f1d9bd4cf9d
Megha Mehta <mmehta.ten@gmail.com>
2022-01-15 11:01:07 +0530

    add method calcArea

--- python/Ch 2/abstract_start.py
+++ python/Ch 2/abstract_start.py
@@ -17,15 +17,21 @@ class Circle(GraphicShape):
     def __init__(self, radius):
         self.radius = radius
 
+    def calcArea(self):
+        return 3.14 * (self.radius ** 2)
+
 
 class Square(GraphicShape):
     def __init__(self, side):
         self.side = side
 
+    def calcArea(self):
+        return (self.side ** 2)
+
 # MEGHA: Comment below because GraphicShape class cannot be instantiated
 # g = GraphicShape()
 
 c = Circle(10)
-print(c.calcArea())
+print("Area of circle: ",c.calcArea())
 s = Square(12)
-print(s.calcArea())
+print("Area of square: ",s.calcArea())
